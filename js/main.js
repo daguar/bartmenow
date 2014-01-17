c4df4b9697fbb4204b00d561a65dd1ab791a22c4
Dave Guarino <dave@codeforamerica.org>
2014-01-17 01:30:33 -0800

    add it all for test

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+// Get current location
+// Find closest BART station
+// Redirect to URL for station
+
+var regExp = /\(([^)]+)\)/;
+var compressed_stations;
+var bart_stations;
+var my_location;
+
+function doShit(currentPosition) {
+	my_location = {};
+	my_location = { latitude: currentPosition.coords.latitude, longitude: currentPosition.coords.longitude }
+	$.getJSON("js/bart_stations.geojson", function(bart_stations) {
+		compressed_stations = {};
+		_.each(bart_stations.features, function(f) {
+			name = regExp.exec(f.properties.name)[1]
+			latlng = { latitude: f.geometry.coordinates[1], longitude: f.geometry.coordinates[0] }
+			compressed_stations[name] = latlng
+		})
+		nearest_station_acronym = geolib.findNearest(my_location, compressed_stations).key
+		window.location.href = "http://m.bart.gov/schedules/eta?stn=" + nearest_station_acronym;
+	})
+}
+
+navigator.geolocation.getCurrentPosition(doShit);
